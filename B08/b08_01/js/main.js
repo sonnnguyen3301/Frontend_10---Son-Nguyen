746b1e87af2bea6ccb7460b75f2ecf4233043e25
unknown <sonnnguyen3301@gmail.com>
2021-12-07 19:36:33 +0700

    b08_01

---
+++ B08/b08_01/js/main.js
@@ -0,0 +1,10 @@
+var dis = 9
+var string = " "
+for (let i = 0; i <= dis; i++) {
+    for (let j = i ; j<=dis; j++)
+    {
+        document.write(j + string)
+    }
+    document.write("<br />")
+    // text += cars[i] + "<br>";    
+  }
